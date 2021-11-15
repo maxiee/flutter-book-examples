3923a4f395c741b83eb9d1b767a8570e5e0c2832
Lu Bin <bbyan2006@live.cn>
2021-11-15 23:08:10 +0800

    适配Dart 2的空安全声明

--- chapter2_tomato/lib/main.dart
+++ chapter2_tomato/lib/main.dart
@@ -24,10 +24,17 @@ class MyHomePage extends StatefulWidget {
 class _MyHomePageState extends State<MyHomePage> {
   static const DEFAULT_COUNT_DOWN = Duration.secondsPerMinute * 25;
   int countDown = 0;
-  Timer timer;
+  /* flutter2.0添加了Sound null safety空安全声明，目的是通过显式声明可能为null的变量，以减少不必要的麻烦。
+  参考资料：https://proxify.io/articles/flutter-2-null-safety
+  **/
+  //Timer timer;
+  // 使用方法是在类型声明后添加?以标识这个对象（变量）是可以为null的
+  Timer? timer;
 
   void startCountDown() {
-    if (timer != null) timer.cancel();
+    // if (timer != null) timer.cancel();
+    //  使用的时候，后面跟!标记
+    if (timer != null) timer!.cancel();
     countDown = DEFAULT_COUNT_DOWN;
     timer = Timer.periodic(Duration(seconds: 1), (timer) {
       setState(() {
